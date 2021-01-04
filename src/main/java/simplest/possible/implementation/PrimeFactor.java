4984fc8644e32a816ba27c745ea6e0fcd03efa70
thilakram <76489522+thilakram@users.noreply.github.com>
2021-01-04 16:16:59 +0530

    Create PrimeFactor.java

---
+++ src/main/java/simplest/possible/implementation/PrimeFactor.java
@@ -0,0 +1,10 @@
+import java.util.Collections;
+import java.util.List;
+
+public class PrimeFactors {
+
+    public List<Integer> of(int number) {
+        
+        return Collections.emptyList();
+    }
+}
